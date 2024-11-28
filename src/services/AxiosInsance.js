0a35e85a77502b6e0a0859072c6be61a047c14c7
salvatoluice <salvatoluice5@gmail.com>
2024-11-28 11:57:11 +0300

    encrypt access token & user data

--- src/services/AxiosInsance.js
+++ src/services/AxiosInsance.js
@@ -1,8 +1,22 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
+import CryptoJS from 'crypto-js';
+
+const SECRET_KEY = import.meta.env.VITE_SECRET_KEY;
+
+const decryptData = (encryptedData) => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(encryptedData, SECRET_KEY);
+    const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+    return JSON.parse(decryptedData);
+  } catch (error) {
+    console.error('Failed to decrypt data:', error);
+    return null;
+  }
+};
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -11,9 +25,12 @@ const api = axios.create({
 // Axios instance for making authenticated requests
 api.interceptors.request.use(
   (config) => {
-    const token = Cookies.get('token') || '';
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+    const encryptedToken = Cookies.get('token');
+    if (encryptedToken) {
+      const token = decryptData(encryptedToken);
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
     }
     return config;
   },
